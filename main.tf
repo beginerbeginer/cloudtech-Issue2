d040c6c428fbf2c3e61a600270c3aee4c138c9ef
beginerbeginer <miyabi6125@gmail.com>
2023-06-18 15:50:22 +0900

    [#1] 追加：route tablesの設定

--- main.tf
+++ main.tf
@@ -94,3 +94,58 @@ resource "aws_subnet" "private_subnet2_c" {
     Name = "pri-sn2-c"
   }
 }
+
+###################
+# route tables
+###################
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "rt-public"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_subnet_c.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "rt-private"
+  }
+}
+
+
+resource "aws_route_table_association" "private1_a" {
+  subnet_id      = aws_subnet.private_subnet1_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private2_a" {
+  subnet_id      = aws_subnet.private_subnet2_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private1_c" {
+  subnet_id      = aws_subnet.private_subnet1_c.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private2_c" {
+  subnet_id      = aws_subnet.private_subnet2_c.id
+  route_table_id = aws_route_table.private_route_table.id
+}
