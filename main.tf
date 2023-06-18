66bec82f8e5c12319a3ccf11bbc4d63efce85479
beginerbeginer <miyabi6125@gmail.com>
2023-06-18 16:44:49 +0900

    [#1] 修正：network ACLs

--- main.tf
+++ main.tf
@@ -154,8 +154,8 @@ resource "aws_route_table_association" "private2_c" {
 # network ACLs
 ###################
 
-# Public Subnet
-resource "aws_network_acl" "public_subnet_acl" {
+# ap-northeast-1a Public Subnet
+resource "aws_network_acl" "public_subnet_acl_a" {
   vpc_id = aws_vpc.my_vpc.id
 
   egress {
@@ -179,12 +179,41 @@ resource "aws_network_acl" "public_subnet_acl" {
   subnet_ids = [aws_subnet.public_subnet_a.id]
 
   tags = {
-    Name = "acl-public"
+    Name = "acl-public-a"
   }
 }
 
-# Private Subnet 1
-resource "aws_network_acl" "private_subnet1_acl" {
+# ap-northeast-1c Public Subnet
+resource "aws_network_acl" "public_subnet_acl_c" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  subnet_ids = [aws_subnet.public_subnet_c.id]
+
+  tags = {
+    Name = "acl-public-c"
+  }
+}
+
+# ap-northeast-1a Private Subnet 1
+resource "aws_network_acl" "private_subnet1_acl_a" {
   vpc_id = aws_vpc.my_vpc.id
 
   egress {
@@ -226,12 +255,59 @@ resource "aws_network_acl" "private_subnet1_acl" {
   subnet_ids = [aws_subnet.private_subnet1_a.id]
 
   tags = {
-    Name = "acl-private1"
+    Name = "acl-private1-a"
   }
 }
 
-# Private Subnet 2
-resource "aws_network_acl" "private_subnet2_acl" {
+# ap-northeast-1c Private Subnet 1
+resource "aws_network_acl" "private_subnet1_acl_c" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_subnet.public_subnet_c.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = aws_subnet.private_subnet2_c.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_subnet.public_subnet_c.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = aws_subnet.private_subnet2_c.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  subnet_ids = [aws_subnet.private_subnet1_c.id]
+
+  tags = {
+    Name = "acl-private1-c"
+  }
+}
+
+# ap-northeast-1a Private Subnet 2
+resource "aws_network_acl" "private_subnet2_acl_a" {
   vpc_id = aws_vpc.my_vpc.id
 
   egress {
@@ -255,6 +331,35 @@ resource "aws_network_acl" "private_subnet2_acl" {
   subnet_ids = [aws_subnet.private_subnet2_a.id]
 
   tags = {
-    Name = "acl-private2"
+    Name = "acl-private2-a"
+  }
+}
+
+# ap-northeast-1c Private Subnet 2
+resource "aws_network_acl" "private_subnet2_acl_c" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_subnet.private_subnet1_c.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_subnet.private_subnet1_c.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+
+  subnet_ids = [aws_subnet.private_subnet2_c.id]
+
+  tags = {
+    Name = "acl-private2-c"
   }
 }
